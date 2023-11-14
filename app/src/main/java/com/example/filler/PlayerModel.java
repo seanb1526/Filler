99dcb2f7519e8f72cd8c4d6bf5d9e4a525639d68
Sean Berndlmaier <138079299+seanb1526@users.noreply.github.com>
2023-11-13 22:50:21 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/filler/PlayerModel.java
@@ -0,0 +1,16 @@
+package com.example.filler;
+
+import android.graphics.Color;
+import android.widget.GridLayout;
+import android.widget.RadioButton;
+import android.widget.CheckBox;
+import android.widget.Button;
+import java.util.Random;
+
+//Model for MVC
+public class PlayerModel {
+    public CheckBox[][] playerBoard;  // personal player board
+
+
+
+}
