a1457ae4575a1ec0eb2358b08f888dd1f1cdd528
thejohnmeyers <jmeyers5@gulls.salisbury.edu>
2023-11-19 19:30:46 -0500

    adding some logic and alot if

--- app/src/main/java/com/example/filler/MainActivity.java
+++ app/src/main/java/com/example/filler/MainActivity.java
@@ -10,22 +10,25 @@ import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     PlayerModel player1 = new PlayerModel();
     Random rand;
+    GridLayout gameBoard;
+    LinearLayout buttonsLay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        player1.playerBoard = new TextView[8][8]; // create a player using model
+        player1.playerBoard = new String[8][8]; // create a player using model
 
         //initialize and create game board - change this later because players will share the game board
-        GridLayout gameBoard = findViewById(R.id.gridLayout);
-        LinearLayout buttonslay = findViewById(R.id.buttonslay);
+        gameBoard = findViewById(R.id.gridLayout);
+        buttonsLay = findViewById(R.id.buttonslay);
         int[] colors = {R.color.color1, R.color.color2, R.color.color3,R.color.color4,
                 R.color.color5, R.color.color6};
         for(int i = 0; i<8; i++){
@@ -42,10 +45,21 @@ public class MainActivity extends AppCompatActivity {
                 //rBtn.setLayoutParams(new GridLayout.LayoutParams());
                 gameBoard.addView(tv);
                 tv.setId(Integer.parseInt(i+ "" + j));
-                player1.playerBoard[i][j] = tv;
+                player1.playerBoard[i][j] = (i + "" + j);
+
             }
         }
 
+    }
+    public void CheckCoords(int x, int y){
+        if(Objects.equals(player1.playerBoard[x][y], "1") || Objects.equals(player1.playerBoard[x][y], "2")){
+            return;
+        }
+        else{
+            int index = (8*x+y)-1; //weird math for index
+            TextView gridChild = (TextView) gameBoard.getChildAt(index);
+            //set color of the above textView
+        }
     }
 
 }
--- app/src/main/java/com/example/filler/PlayerModel.java
+++ app/src/main/java/com/example/filler/PlayerModel.java
@@ -6,12 +6,57 @@ import android.widget.RadioButton;
 import android.widget.CheckBox;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Objects;
 import java.util.Random;
 
 //Model for MVC
 public class PlayerModel {
-    public TextView[][] playerBoard;  // personal player board
+    public String[][] playerBoard;  // personal player board
+    int colorSelected;
+    int turn;
+    public void StartGame(){
+        playerBoard[0][7] = "2";
+        playerBoard[7][0] = "1";
+        GameLoop();
+    }
+    public void GameLoop()
+    {
 
+    }
 
+    public String[] SearchBoard(){
 
+        for(int row = 0; row < 8; row++){
+            for(int col = 0; col < 8; col++){
+                if(Objects.equals(playerBoard[row][col], "1") || Objects.equals(playerBoard[row][col], "2")){
+                    if(row == 0 && col == 0){
+                        //check/set the color to the right and down to new color
+                    }
+                    //might take the two starting ones out we'll see
+                    else if(row == 0 && col == 7){
+                        //check/set color down and left
+                    }
+                    else if(row == 7 && col == 0){
+                        //check/set color up and right
+                    }
+                    else if(row == 7 && col == 7){
+                        //check/set color to the left and up one
+                    }
+                    else if(row == 0){
+                        //check/set color to the right left and down
+                    }
+                    else if(col == 0){
+                        //check/set color to the right and up
+                    }
+                    else if(row == 7){
+                        //check/set color to the up and right
+                    }
+                    else if(col == 7){
+                        //check/set color to the left and down
+                    }
+                }
+            }
+        }
+    }
 }
