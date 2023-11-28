6e29e316110442150f89c3271751b6219346fe23
Sean Berndlmaier <sberndlmaier@proton.me>
2023-11-28 10:16:08 -0500

    Added Home Activity with functioning start button

--- app/src/main/java/com/example/filler/HomeScreen.java
+++ app/src/main/java/com/example/filler/HomeScreen.java
@@ -2,7 +2,9 @@ package com.example.filler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 // this is the first activity. It will be the default first screen when app is run.
 public class HomeScreen extends AppCompatActivity {
@@ -11,5 +13,11 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
+
+
+    public void clicked(View view) {
+        Intent intent = new Intent(HomeScreen.this, MainActivity.class);
+        startActivity(intent);
     }
 }
