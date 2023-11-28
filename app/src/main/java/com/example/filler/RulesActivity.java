89aeec105ab9a202c01cc1567ceadd6bbf4d9486
Sean Berndlmaier <sberndlmaier@proton.me>
2023-11-28 11:04:35 -0500

    Added Rules activity and updated color values

---
+++ app/src/main/java/com/example/filler/RulesActivity.java
@@ -0,0 +1,15 @@
+package com.example.filler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// Activity to display rules/How to play  -> HomeScreen -> Rules -> Main
+public class RulesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+    }
+}
