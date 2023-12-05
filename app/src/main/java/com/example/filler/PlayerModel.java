0dc3e32a5d978965ca8396476dd510d23e682652
Sean Berndlmaier <sberndlmaier@proton.me>
2023-12-04 21:13:08 -0500

    update

--- app/src/main/java/com/example/filler/PlayerModel.java
+++ app/src/main/java/com/example/filler/PlayerModel.java
@@ -6,12 +6,49 @@ import android.widget.RadioButton;
 import android.widget.CheckBox;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Objects;
 import java.util.Random;
 
 //Model for MVC
 public class PlayerModel {
-    public TextView[][] playerBoard;  // personal player board
+    public int[][] playerBoard;  // personal player board
+    int colorSelected;
+    int turn;
+    public PlayerModel(){
+        playerBoard = new int[8][8];
+    }
+    public void StartGame(){
+        // playerBoard[0][7] = "2";
+        playerBoard[7][0] = 1;
+        GameLoop();
+    }
+    public void setBoardLoc(int x, int y, int change){
+        playerBoard[x][y] = change;
+    }
+    public int[][] getBoard(){
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
 
+                System.out.print(playerBoard[i][j]);
+            }
+            System.out.println();
+        }
+        return playerBoard;
+    }
+    public void intitializeBoard(){
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                playerBoard[i][j] = 0;
+                System.out.print(playerBoard[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    public void GameLoop()
+    {
+
+    }
 
 
 }
